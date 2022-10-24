153539b5f12ecfaeed2d3f2c000f51bbdca6f601
Greg Gajek <greg@sycle.net>
2022-10-25 01:01:32 +0200

    backing up

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var currentChord = [];
 var currentStep = 0;
 var correctNote = 0;
 var correctType = '';
+var selectedChords = [];
+var selectedTypes = [];
 var chordSymbols = [
   {
     "name": 'C',
@@ -108,11 +110,12 @@ function initForm() {
   var rootUl = document.getElementById('rootUl');
   for (let i=0; i<chordSymbols.length; i++) {    
     var newLi = document.createElement('li');
+    newLi.setAttribute('class', 'tiles');
     var newInput = document.createElement('input');
     newInput.setAttribute('type', 'checkbox');
     newInput.setAttribute('name', 'roots[]');
-    newInput.setAttribute('checked', 'checked');
-    newInput.setAttribute('value', chordSymbols[i].note);
+    // newInput.setAttribute('checked', 'checked');
+    newInput.setAttribute('value', i);
     newInput.setAttribute('id', chordSymbols[i].name);
     var newLabel = document.createElement('label');
     newLabel.setAttribute('for', chordSymbols[i].name);
@@ -127,8 +130,8 @@ function initForm() {
     var newInput = document.createElement('input');
     newInput.setAttribute('type', 'checkbox');
     newInput.setAttribute('name', 'types[]');
-    newInput.setAttribute('checked', 'checked');
-    newInput.setAttribute('value', chordTypes[i].note);
+    // newInput.setAttribute('checked', 'checked');
+    newInput.setAttribute('value', i);
     newInput.setAttribute('id', 'type'+i);
     var newLabel = document.createElement('label');
     newLabel.setAttribute('for', 'type'+i);
@@ -137,6 +140,43 @@ function initForm() {
     newLi.appendChild(newLabel);
     typeUl.appendChild(newLi);
   }
+}
+
+function initGreg() {
+  let notes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  let ctypes = [0, 1, 2, 3];
+  let formRoots = document.getElementsByName('roots[]');
+  let formTypes = document.getElementsByName('types[]');
+  formRoots.forEach((root) => {
+    root.checked = notes.includes(parseInt(root.value));
+  });
+  formTypes.forEach((ctype) => {
+    ctype.checked = ctypes.includes(parseInt(ctype.value));
+  });  
+}
+
+function initJola() {
+  let notes = [0, 1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13];
+  let ctypes = [0, 1];
+  let formRoots = document.getElementsByName('roots[]');
+  let formTypes = document.getElementsByName('types[]');
+  formRoots.forEach((root) => {
+    root.checked = notes.includes(parseInt(root.value));
+  });
+  formTypes.forEach((ctype) => {
+    ctype.checked = ctypes.includes(parseInt(ctype.value));
+  });  
+}
+
+function resetForm() {
+  let formRoots = document.getElementsByName('roots[]');
+  let formTypes = document.getElementsByName('types[]');
+  formRoots.forEach((root) => {
+    root.checked = false;
+  });
+  formTypes.forEach((ctype) => {
+    ctype.checked = false;
+  });  
 }
 
 function onMIDIFailure() {
@@ -176,23 +216,34 @@ function noteOnListener(note) {
     int1 = currentChord[1]-currentChord[0];
     int2 = currentChord[2]-currentChord[1];
     tonicNote = currentChord[correctType.tonicIndex];
-    // console.log(rootNote % 12 == correctNote);
-    console.log(JSON.stringify(Array(int1, int2)));
-    // console.log(JSON.stringify(correctType.intervals));
-    // document.querySelector('#info').innerText = int1+"-"+int2;
+    console.log('currentChord '+currentChord+" intervals"+int1+","+int2," correct intervals:"+correctType.intervals);
+    console.log("tonicNote:"+tonicNote+" correctNote:"+correctNote);
     if ( (tonicNote % 12 == correctNote) && JSON.stringify(Array(int1, int2)) == JSON.stringify(correctType.intervals) ) {
       gameStep();
     }     
   }
-//  document.querySelector('#info').innerText = currentChord;
 }
 
 function noteOffListener(note) {
   currentChord.splice(currentChord.indexOf(note), 1);
-  // document.querySelector('#info').innerText = '';
 }
 
 function startGame() {
+  let formRoots = document.getElementsByName('roots[]');
+  selectedChords = []
+  formRoots.forEach((root) => {
+    if (root.checked) {
+      selectedChords.push(chordSymbols[root.value]);
+    }
+  });
+  let formTypes = document.getElementsByName('types[]');
+  selectedTypes = [];
+  formTypes.forEach((ctype) => {
+    if (ctype.checked) {
+      selectedTypes.push(chordTypes[ctype.value]);
+    }
+  });
+  if ((selectedChords.length == 0) || (selectedTypes.length == 0)) return false;
   document.querySelector('#formdiv').style.display = 'none';
   document.querySelector('#gamediv').style.display = 'block';
   gameStep();
@@ -205,10 +256,17 @@ function resetGame() {
 }
 
 function gameStep() {
-  randSymbol = Math.floor(Math.random() * chordSymbols.length);
-  randType = Math.floor(Math.random() * chordTypes.length);
-  correctNote = chordSymbols[randSymbol].note;
-  correctType = chordTypes[randType];
-  document.querySelector('#question').innerText = chordSymbols[randSymbol].name+"-"+chordTypes[randType].name;
+  randSymbol = Math.floor(Math.random() * selectedChords.length);
+  randType = Math.floor(Math.random() * selectedTypes.length);
+  correctNote = selectedChords[randSymbol].note;
+  correctType = selectedTypes[randType];
+  correctNotes = [correctNote];
+  let y  = correctNote;
+  for (let i=0; i<correctType.intervals.length; i++) {
+    correctNotes.push(y+correctType.intervals[i]);
+    y += correctType.intervals[i];
+  }
+  console.log("correctNotes:"+correctNotes);
+  document.querySelector('#question').innerText = selectedChords[randSymbol].name+"-"+selectedTypes[randType].name.replace(" ", "\n");
   currentStep++;
 }
