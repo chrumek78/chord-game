3d6008bcf7caf6e82fd49c66c44066d02405fd61
Greg Gajek <greg@sycle.net>
2022-10-25 12:50:44 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var currentChord = [];
 var currentStep = 0;
-var correctNote = 0;
-var correctType = '';
+var currentNotes = [];
+var currentBass = 0;
+var correctNotes = [];
+var correctBass = 0;
 var selectedChords = [];
 var selectedTypes = [];
-var chordSymbols = [
+var selectedInversions = [];
+var chordRoots = [
   {
     "name": 'C',
     "note": 0
@@ -30,6 +32,10 @@ var chordSymbols = [
     "note": 4
   },
   {
+    "name": "Fes",
+    "note": 4
+  },
+  {
     "name": 'F',
     "note": 5
   },
@@ -44,6 +50,10 @@ var chordSymbols = [
   {
     "name": 'G',
     "note": 7
+  },
+  {
+    "name": 'Gis',
+    "note": 8
   },
   {
     "name": 'As',
@@ -69,34 +79,32 @@ var chordSymbols = [
 
 var chordTypes = [
   {
-    "name": 'dur postać zasadnicza',
+    "name": 'dur',
     "intervals": [4, 3],
-    "tonicIndex": 0
   },
   {
-    "name": 'moll postać zasadnicza',
+    "name": 'moll',
     "intervals": [3, 4],
-    "tonicIndex": 0
   },
   {
-    "name": 'dur I przewrót',
-    "intervals": [3, 5],
-    "tonicIndex": 2
+    "name": 'dim',
+    "intervals": [3, 3],
   },
   {
-    "name": 'moll I przewrót',
-    "intervals": [4, 5],
-    "tonicIndex": 2
+    "name": 'aug',
+    "intervals": [4, 4],
   },
   {
-    "name": 'dur II przewrót',
-    "intervals": [5, 4],
-    "tonicIndex": 1
+    "name": '7',
+    "intervals": [4, 3, 3],
   },
   {
-    "name": 'moll II przewrót',
-    "intervals": [5, 3],
-    "tonicIndex": 1
+    "name": 'm7',
+    "intervals": [3, 4, 3],
+  },
+  {
+    "name": 'M7',
+    "intervals": [4, 3, 4],
   },
 ];
 
@@ -106,20 +114,27 @@ if (navigator.requestMIDIAccess) {
   document.querySelector('#gamediv').innerText = "This Browser doesn't support WebMIDI";
 }
 
+function inArray(needle, haystack) {
+  var length = haystack.length;
+  for(var i = 0; i < length; i++) {
+      if(haystack[i] == needle) return true;
+  }
+  return false;
+}
+
 function initForm() {
   var rootUl = document.getElementById('rootUl');
-  for (let i=0; i<chordSymbols.length; i++) {    
+  for (let i=0; i<chordRoots.length; i++) {    
     var newLi = document.createElement('li');
     newLi.setAttribute('class', 'tiles');
     var newInput = document.createElement('input');
     newInput.setAttribute('type', 'checkbox');
     newInput.setAttribute('name', 'roots[]');
-    // newInput.setAttribute('checked', 'checked');
     newInput.setAttribute('value', i);
-    newInput.setAttribute('id', chordSymbols[i].name);
+    newInput.setAttribute('id', chordRoots[i].name);
     var newLabel = document.createElement('label');
-    newLabel.setAttribute('for', chordSymbols[i].name);
-    newLabel.innerHTML = chordSymbols[i].name;
+    newLabel.setAttribute('for', chordRoots[i].name);
+    newLabel.innerHTML = chordRoots[i].name;
     newLi.appendChild(newInput);
     newLi.appendChild(newLabel);
     rootUl.appendChild(newLi);
@@ -127,10 +142,10 @@ function initForm() {
   var typeUl = document.getElementById('typeUl');
   for (let i=0; i<chordTypes.length; i++) {
     var newLi = document.createElement('li');
+    newLi.setAttribute('class', 'tiles');
     var newInput = document.createElement('input');
     newInput.setAttribute('type', 'checkbox');
     newInput.setAttribute('name', 'types[]');
-    // newInput.setAttribute('checked', 'checked');
     newInput.setAttribute('value', i);
     newInput.setAttribute('id', 'type'+i);
     var newLabel = document.createElement('label');
@@ -142,41 +157,31 @@ function initForm() {
   }
 }
 
-function initGreg() {
-  let notes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  let ctypes = [0, 1, 2, 3];
+function initCheckboxes(notes, types, inversions) {
   let formRoots = document.getElementsByName('roots[]');
   let formTypes = document.getElementsByName('types[]');
+  let formInv = document.getElementsByName('inversions[]');
   formRoots.forEach((root) => {
     root.checked = notes.includes(parseInt(root.value));
   });
   formTypes.forEach((ctype) => {
-    ctype.checked = ctypes.includes(parseInt(ctype.value));
+    ctype.checked = types.includes(parseInt(ctype.value));
   });  
+  formInv.forEach((invtype) => {
+    invtype.checked = inversions.includes(parseInt(invtype.value))
+  });  
+}
+
+function initGreg() {
+  initCheckboxes([0, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 16], [0, 1], [0, 1, 2, 3]);
 }
 
 function initJola() {
-  let notes = [0, 1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13];
-  let ctypes = [0, 1];
-  let formRoots = document.getElementsByName('roots[]');
-  let formTypes = document.getElementsByName('types[]');
-  formRoots.forEach((root) => {
-    root.checked = notes.includes(parseInt(root.value));
-  });
-  formTypes.forEach((ctype) => {
-    ctype.checked = ctypes.includes(parseInt(ctype.value));
-  });  
+  initCheckboxes([0, 1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13], [0, 1], [0]);
 }
 
 function resetForm() {
-  let formRoots = document.getElementsByName('roots[]');
-  let formTypes = document.getElementsByName('types[]');
-  formRoots.forEach((root) => {
-    root.checked = false;
-  });
-  formTypes.forEach((ctype) => {
-    ctype.checked = false;
-  });  
+  initCheckboxes([], [], []);
 }
 
 function onMIDIFailure() {
@@ -205,35 +210,12 @@ function getMIDIMessage(message) {
   }
 }
 
-function noteOnListener(note) {
-  if (currentStep == 0) {
-    document.querySelector('#question').innerText = '';
-    gameStep();
-  }
-  currentChord.push(note)
-  currentChord.sort();
-  if (currentChord.length == 3) {
-    int1 = currentChord[1]-currentChord[0];
-    int2 = currentChord[2]-currentChord[1];
-    tonicNote = currentChord[correctType.tonicIndex];
-    console.log('currentChord '+currentChord+" intervals"+int1+","+int2," correct intervals:"+correctType.intervals);
-    console.log("tonicNote:"+tonicNote+" correctNote:"+correctNote);
-    if ( (tonicNote % 12 == correctNote) && JSON.stringify(Array(int1, int2)) == JSON.stringify(correctType.intervals) ) {
-      gameStep();
-    }     
-  }
-}
-
-function noteOffListener(note) {
-  currentChord.splice(currentChord.indexOf(note), 1);
-}
-
 function startGame() {
   let formRoots = document.getElementsByName('roots[]');
   selectedChords = []
   formRoots.forEach((root) => {
     if (root.checked) {
-      selectedChords.push(chordSymbols[root.value]);
+      selectedChords.push(chordRoots[root.value]);
     }
   });
   let formTypes = document.getElementsByName('types[]');
@@ -243,7 +225,14 @@ function startGame() {
       selectedTypes.push(chordTypes[ctype.value]);
     }
   });
-  if ((selectedChords.length == 0) || (selectedTypes.length == 0)) return false;
+  let formInv = document.getElementsByName('inversions[]');
+  selectedInversions = [];
+  formInv.forEach((inv) => {
+    if (inv.checked) {
+      selectedInversions.push(inv.value);
+    }
+  });
+  if ((selectedChords.length == 0) || (selectedTypes.length == 0) || (selectedInversions.length == 0)) return false;
   document.querySelector('#formdiv').style.display = 'none';
   document.querySelector('#gamediv').style.display = 'block';
   gameStep();
@@ -258,15 +247,61 @@ function resetGame() {
 function gameStep() {
   randSymbol = Math.floor(Math.random() * selectedChords.length);
   randType = Math.floor(Math.random() * selectedTypes.length);
-  correctNote = selectedChords[randSymbol].note;
+  // filter out impossible inversions
+  let availableInversions = [...selectedInversions];
+  console.log("availableInversions "+availableInversions);
+  if (selectedTypes[randType].intervals.length < 3) {
+    if (availableInversions.indexOf('3')>0) {
+      availableInversions.splice(availableInversions.indexOf('3'), 1);
+    }
+  }
+  invIndex = Math.floor(Math.random() * availableInversions.length);
+  console.log("availableInversions "+availableInversions);
+  console.log("availableInversions.length "+availableInversions.length);
+  console.log("invIndex "+invIndex);
+  if (invIndex == 0) {
+    randInversion = 0;
+  } else {  
+    randInversion = availableInversions[invIndex];
+  }
+  correctRoot = selectedChords[randSymbol].note;
   correctType = selectedTypes[randType];
-  correctNotes = [correctNote];
-  let y  = correctNote;
+  correctNotes = [correctRoot];
+  let y  = correctRoot;
   for (let i=0; i<correctType.intervals.length; i++) {
-    correctNotes.push(y+correctType.intervals[i]);
+    correctNotes.push((y+correctType.intervals[i]) % 12);
     y += correctType.intervals[i];
   }
-  console.log("correctNotes:"+correctNotes);
-  document.querySelector('#question').innerText = selectedChords[randSymbol].name+"-"+selectedTypes[randType].name.replace(" ", "\n");
+  correctBass = correctNotes[randInversion];
+  correctNotes.sort(function(a, b){return a-b});
+  invText = randInversion>0 ? randInversion+" przewrót" : "postać zasadnicza";
+  document.querySelector('#question').innerText = selectedChords[randSymbol].name+"-"+selectedTypes[randType].name+" "+invText;
   currentStep++;
+}
+
+function noteOnListener(note) {
+  if (!currentStep) return false;
+  currentNotes.push(note);
+  currentBass = Math.min(...currentNotes) % 12;
+  let correctCount = 0;
+  for (let i=0; i<currentNotes.length; i++) {
+    if (correctNotes.includes(currentNotes[i] % 12)) {
+      correctCount++;
+    }
+  }
+  document.getElementById('result').innerText = '✅ '.repeat(correctCount);
+  if ( (correctCount == correctNotes.length) && (currentBass == correctBass) ) {
+    gameStep();
+  }
+}
+
+function noteOffListener(note) {
+  currentNotes.splice(currentNotes.indexOf(note), 1);
+  let correctCount = 0;
+  for (let i=0; i<currentNotes.length; i++) {
+    if (correctNotes.includes(currentNotes[i] % 12)) {
+      correctCount++;
+    }
+  }
+  document.getElementById('result').innerText = '✅ '.repeat(correctCount);
 }
