3bec571789de358160b983b6b17595311b0b37c1
Greg Gajek <greg@sycle.net>
2022-12-03 01:12:16 +0100

    adding gege3 preset

--- main.js
+++ main.js
@@ -259,6 +259,9 @@ function initGreg2() {
   initCheckboxes([0, 3, 5, 7, 10, 13, 15], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], ['connected1']);
 }
 
+function initGreg3() {
+  initCheckboxes([0, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17], ['connected1']);
+}
 function initJola() {
   initCheckboxes([0, 1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13], [0, 1], [0]);
 }
